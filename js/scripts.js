fd45e49ef32190e7270cdb4198d31fe005b80998
Nikhilkumar <P2424383@my365.dmu.ac.uk>
2021-04-24 16:42:24 +0100

    remote not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-console.log("hello");
+menuToggler.addEventListener('click', ev => {
+  menu.classList.toggle('open');
+  menuToggler.textContent = menuToggler.textContent === "x" ? "≡" : "x";
+});
+
+
+"use strict";
+
+const checkConfirmation = (ev) => {
+ if (myPassword.value != myConfirmation.value) {
+  myConfirmation.setCustomValidity("Passwords do not match. Please try again!");
+ } else {
+  myConfirmation.setCustomValidity('');
+ }
+};
+
+function ValidateEmail(myEmail)
+{
+ if (myEmail
+
+myConfirmation.addEventListener('input', checkConfirmation);
+myPassword.addEventListener('input', checkConfirmation);
+
+login.addEventListener('submit', ev => {
+ alert(`logged in as ${myEmail.value}`)
+ login.reset();
+ ev.preventDefault();
+});
